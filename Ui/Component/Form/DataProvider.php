6793691edcb0d6243db0b7627e6b0352e88b5cb7
Tiago Sampaio <tiago@tiagosampaio.com>
2018-05-22 15:24:43 -0300

    Some fixes in the UiComponent rendering.

---
+++ Ui/Component/Form/DataProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BitTools\SkyHub\Ui\Component\Form;
+
+use Magento\Ui\DataProvider\AbstractDataProvider;
+
+class DataProvider extends AbstractDataProvider
+{
+    
+    /**
+     * @param \Magento\Framework\Api\Filter $filter
+     *
+     * @return void
+     */
+    public function addFilter(\Magento\Framework\Api\Filter $filter)
+    {
+        /** We've overridden this method because this class does not use a collection to be rendered. */
+    }
+    
+    
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        /** This method also does not need any data to be loaded. */
+        return [];
+    }
+}
